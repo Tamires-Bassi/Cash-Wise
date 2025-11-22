41ac95953a8eb6e068045333ede3bc11143cc4f9
Tamires Bassi <tamires.bassi@fatec.sp.gov.br>
2025-11-22 07:42:19 -0300

    feat: add account renaming functionality

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart'; // import do Flutter material
-import 'package:firebase_auth/firebase_auth.dart'; 
+import 'package:firebase_auth/firebase_auth.dart'; // 
 
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
